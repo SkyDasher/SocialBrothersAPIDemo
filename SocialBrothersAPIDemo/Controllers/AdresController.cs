796f23713fd71ac45a2ceb66e88d20f1783ba020
SkyDasher <KBosch_12@hotmail.com>
2021-09-09 20:30:05 +0200

    last push for kilometres api and last touches readme

--- SocialBrothersAPIDemo/Controllers/AdresController.cs
+++ SocialBrothersAPIDemo/Controllers/AdresController.cs
@@ -8,6 +8,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net;
+using System.Xml;
 
 namespace SocialBrothersAPIDemo.Controllers
 {
@@ -91,26 +92,117 @@ namespace SocialBrothersAPIDemo.Controllers
         }
         [HttpPost]
         [Route("Kilometers")]
-        public ActionResult GetKilometresBetweenAdressen(Dictionary<string , Adres> data)
+        public string GetKilometresBetweenAdressen(Dictionary<string , Adres> data)
         {
             AdresDto adres1 = data["adres1"].AsDto();
             AdresDto adres2 = data["adres2"].AsDto();
 
-            var beginlatlongurl = String.Format("http://dev.virtualearth.net/REST/v1/Locations?countryRegion=" + adres1.Land + "&adminDistrict="+adres1.Plaats+"&locality="+adres1.Plaats+"&postalCode="+adres1.Postcode+"&addressLine="+adres1.Straat+adres1.Huisnummer+"&maxResults=1&o=xml&key="+bingKey);
-            WebRequest requestObj1 = WebRequest.Create(beginlatlongurl);
+            var beginlatlongurl1 = String.Format("http://dev.virtualearth.net/REST/v1/Locations?countryRegion=" + adres1.Land + "&adminDistrict="+adres1.Plaats+"&locality="+adres1.Plaats+"&postalCode="+adres1.Postcode+"&addressLine="+adres1.Straat+adres1.Huisnummer+"&maxResults=1&o=xml&key="+bingKey);
+            WebRequest requestObj1 = WebRequest.Create(beginlatlongurl1);
 
             HttpWebResponse respobj1 = null;
             respobj1 = (HttpWebResponse)requestObj1.GetResponse();
 
             string strresult1 = null;
+            XmlDocument xmlDoc1 = new XmlDocument();
             using (Stream stream = respobj1.GetResponseStream())
             {
                 StreamReader sr = new StreamReader(stream);
                 strresult1 = sr.ReadToEnd();
+                xmlDoc1.LoadXml(strresult1);
                 sr.Close();
             }
 
-            return View(strresult1);
+            var lat1 = xmlDoc1.GetElementsByTagName("Latitude");
+            var long1 = xmlDoc1.GetElementsByTagName("Longitude");
+
+            var latitude1 = "";
+            if(lat1 != null)
+            {
+                foreach(XmlNode curr in lat1)
+                {
+                    latitude1 = curr.InnerText;
+                }
+            }
+
+            var longitude1 = "";
+            if (long1 != null)
+            {
+                foreach (XmlNode curr in long1)
+                {
+                    longitude1 = curr.InnerText;
+                }
+            }
+
+            string latlong1 = latitude1 +","+ longitude1;
+
+            var beginlatlongurl2 = String.Format("http://dev.virtualearth.net/REST/v1/Locations?countryRegion=" + adres2.Land + "&adminDistrict=" + adres2.Plaats + "&locality=" + adres2.Plaats + "&postalCode=" + adres2.Postcode + "&addressLine=" + adres2.Straat + adres2.Huisnummer + "&maxResults=1&o=xml&key=" + bingKey);
+            WebRequest requestObj2 = WebRequest.Create(beginlatlongurl2);
+
+            HttpWebResponse respobj2 = null;
+            respobj2 = (HttpWebResponse)requestObj2.GetResponse();
+
+            string strresult2 = null;
+            XmlDocument xmlDoc2 = new XmlDocument();
+            using (Stream stream = respobj2.GetResponseStream())
+            {
+                StreamReader sr = new StreamReader(stream);
+                strresult2 = sr.ReadToEnd();
+                xmlDoc2.LoadXml(strresult2);
+                sr.Close();
+            }
+
+            var lat2 = xmlDoc2.GetElementsByTagName("Latitude");
+            var long2 = xmlDoc2.GetElementsByTagName("Longitude");
+
+            var latitude2 = "";
+            if (lat2 != null)
+            {
+                foreach (XmlNode curr in lat2)
+                {
+                    latitude2 = curr.InnerText;
+                }
+            }
+
+            var longitude2 = "";
+            if (long2 != null)
+            {
+                foreach (XmlNode curr in long2)
+                {
+                    longitude2 = curr.InnerText;
+                }
+            }
+
+            string latlong2 = latitude2 +","+ longitude2;
+
+            var distanceUrl = String.Format("https://dev.virtualearth.net/REST/v1/Routes/DistanceMatrix?origins="+latlong1+"&destinations="+latlong2+"&travelMode=driving&o=xml&key=" + bingKey);
+            WebRequest distance = WebRequest.Create(distanceUrl);
+
+            HttpWebResponse distanceResp = null;
+            distanceResp = (HttpWebResponse)distance.GetResponse();
+
+            string distRes = null;
+            XmlDocument x = new XmlDocument();
+            using (Stream stream = distanceResp.GetResponseStream())
+            {
+                StreamReader sr = new StreamReader(stream);
+                distRes = sr.ReadToEnd();
+                x.LoadXml(distRes);
+                sr.Close();
+            }
+
+            var kilometers = x.GetElementsByTagName("TravelDistance");
+            var kmbetweenpoints = "";
+            
+            if (kilometers != null)
+            {
+                foreach (XmlNode curr in kilometers)
+                {
+                    kmbetweenpoints = curr.InnerText;
+                }
+            }
+
+            return "Kilometers: " + kmbetweenpoints;
         }
     }
 }
--- SocialBrothersAPIDemo/readme.txt
+++ SocialBrothersAPIDemo/readme.txt
@@ -6,7 +6,27 @@ Deel 2: helaas is niet alles gelukt, alle data staat in een tabel en kan op gezo
 Mijn idee was om met het drukken van het knop van welke rij je wilde sorteren de tabelrijnaam mee te geven met daarbij de vorige sort mogelijkheid (dus 'geen sort', 'asc', 'desc') zodat daar doorheen gedrukt kon worden. 
 De data kon verwerkt worden binnen de controller en weer mee terug gestuurd worden naar de view pagina.
 
-Deel 3: 
+Deel 3: In de swagger UI moet je het volgende kopieren en plakken om daaruit informatie te krijgen.
+
+{
+  "adres1": {
+    "adresId": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+    "straat": "Arkelstraat",
+    "huisnummer": "15",
+    "postcode": "4201 KA",
+    "plaats": "Gorinchem",
+    "land": "Nederland"
+  },
+  "adres2": {
+    "adresId": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+    "straat": "Weeshuissteeg",
+    "huisnummer": "19",
+    "postcode": "1441 CS",
+    "plaats": "Purmerend",
+    "land": "Nederland"
+  }
+}
+
 
 
 Meest tevreden: API's. na best wel een lange tijd sinds mijn stage is het me weer gelukt om een API op te zetten. daar ben ik best trots op en volgens mij is het goed gelukt.
